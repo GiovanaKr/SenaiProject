c8776c9f9eeafaa20436e4528e3cf8c491655d28
céu <60863899+GiovanaKr@users.noreply.github.com>
2021-10-26 20:51:40 -0300

    Update ObjetoPedido.py

--- SenaiPython/ObjetoPedido.py
+++ SenaiPython/ObjetoPedido.py
@@ -2,9 +2,10 @@
 listaP = [] #lista global
  
 class Pedido:   #OBJETO
-    def __init__(self, name):
-        self.name = name
-        self.aprovGen = 2
-        self.arovCom = 2
-        self.logis = 2
-        self.entrega = 0
+    def __init__(self, name, qtd):
+        self.nome = name   #nome item
+        self.qtd = qtd     #quantidade
+        self.aprovGen = 2  #gerente
+        self.arovCom = 2   #compras
+        self.log = 2       #logistica
+        self.entrega = 0   #entregue
