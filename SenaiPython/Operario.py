a2901ac871f095c14a100a88b2c2e7cf68fa8073
céu <60863899+GiovanaKr@users.noreply.github.com>
2021-10-26 20:52:04 -0300

    Update Operario.py

--- SenaiPython/Operario.py
+++ SenaiPython/Operario.py
@@ -1,29 +1,58 @@
 #Operario
 import ObjetoPedido
- 
+import os
+clear = lambda: os.system('cls')
 lista = ObjetoPedido.listaP
+
 class Operario:
     def Main():
-        print("deseja:")
-        r = input("1-fazer pedido   2-checar pedidos")
+        clear()
+        print("#######PERFIL OPERACIONAL#######")
+        print("1-Solicitar produto")
+        print("2-Verificar solicitações")
+        print("5-Logout")
+        r = input(": ")
         if int(r) == 1:
-            print("entrou")
             Operario.CriarPedido()
-        else:
+        elif int(r) == 2:
             Operario.VerLista()
+        elif int(r) == 5:
+            return
+        else:
+            Operario.Main()
  
         
     def CriarPedido(): #cria pedido novo
-        nome = input("nome para o pedido:")
-        pp = ObjetoPedido.Pedido(nome)
-        lista.append(pp)    #chama lista do main
+        clear()
+        nome = input("Item:")
+        qtd = input("Quantidade:")
+        pp = ObjetoPedido.Pedido(nome,qtd)
+        lista.append(pp)
         Operario.Main()
  
-    def VerLista():
+    def VerLista(): #lista pedidos
+        clear()
         for obj in lista:
-            print("Nome pedido:"+obj.name)
-            if obj.aprovGen == 2:
-                print("Aguardando Aprovação")
+            print("Item: "+obj.qtd+" "+obj.nome)
+            
+            if obj.aprovGen == 0:
+                print("Pedido Negado[gerencia]")
+            elif obj.aprovGen == 2:
+                print("Pedido em Exame")
+            elif obj.com == 0:
+                print("Pedido Negado[compras]")
+            elif obj.com == 2:
+                print("[Aprovado gerencia]")
+                print("Pedido em Exame[compras]")
+            elif obj.log == 0:
+                print("Erro no produto[logistica]")
+            elif obj.log == 2:
+                print("[aprovado compras]")
+                print("Espera do produto[logistica]")
+            elif obj.entrega == 0:
+                print("Esperando retirar produto requisitado!")
+            elif obj.entrega == 1:
+                print("Produto Entregue")
+            print("")
+        x = input("")
         Operario.Main()
- 
-Operario.Main()
