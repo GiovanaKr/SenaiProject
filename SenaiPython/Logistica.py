ab2fae104aadf0eae09507c3b6b0575dd788300d
céu <60863899+GiovanaKr@users.noreply.github.com>
2021-11-03 21:24:54 -0300

    Update Logistica.py

--- SenaiPython/Logistica.py
+++ SenaiPython/Logistica.py
@@ -3,29 +3,20 @@ import os
 clear = lambda: os.system('cls')
 lista = ObjetoPedido.listaP
 
-#sair da def lista()
-#input incorreto simples
-#loop lista
-
-#fazer retirada de produto
-
-gg = ObjetoPedido.Pedido('papel','100')
-gg.aprovCom = '1'
-lista.append(gg)
-pp = ObjetoPedido.Pedido('caneta','10')
-pp.aprovCom = '1'
-lista.append(pp)
+#gg = ObjetoPedido.Pedido('papel','100')
+#lista.append(gg)
 
 class Logistica:
     def Main():
         clear()
-        print("#######PERFIL GERENCIAL#######")
+        print("#######PERFIL Logistica#######")
         print("1-Verificar/Modificar solicitações")
         print("2-Retirada de produto")
         print("5-Logout")
         r = input(": ")
         if int(r) == 1:
             Logistica.Lista()
+            Logistica.Main()
         elif int(r) == 2:
             Logistica.Retirada()
         elif int(r) == 5:
@@ -36,11 +27,11 @@ class Logistica:
     def Lista():
         clear()
         h = 0
-        print("pressione 's' para sair")
+        print("pressione '0' para sair")
         print("")
         for x in range(len(lista)): #para cada item na lista
             if int(lista[x].aprovCom) == 1 :
-                print(str(x)+" "+"Item: "+lista[x].qtd+" "+lista[x].nome)
+                print(str(x+1)+" "+"Item: "+lista[x].qtd+" "+lista[x].nome)
                 if int(lista[x].log) == 2:
                     print("Aguardando verificação")
                 elif int(lista[x].log) == 1:
@@ -52,19 +43,22 @@ class Logistica:
 
         if h == 0 :
             print("Aguardando requisições")
+            f = input("")
+            return
         else:
             y = input("Modificar item nº:")
-            if y == 's':
-                Logistica.Main()
+            d = int(y)-1
+            if int(y) == 0:
+                return
             for x in range(len(lista)):
                 if int(lista[x].aprovCom) == 1 :
-                    if x == int(y):
+                    if x == d:
                         print(lista[x].qtd + " " +lista[x].nome)
                         r = input("Recebido(1)   Recusado(0)")
                         if  int(r) == 0:
                             lista[x].justificava=input('Motivo:')
 
-                        if r == 's':
+                        if r == 0:
                             Logistica.Main()
                         if int(r) == 0 or int(r) == 1:
                             lista[x].log = r 
@@ -78,11 +72,11 @@ class Logistica:
                             lista[x].entrega = s
 
         x = input("")
-        Logistica.Lista()
+        Logistica.Main()
 
     def Retirada():
         clear()
-        print("pressione 's' para sair")
+        print("pressione '0' para sair")
         print("")
         h=0
         for x in range(len(lista)):
@@ -91,20 +85,15 @@ class Logistica:
                     print(str(x)+" "+"Item: "+lista[x].qtd+" "+lista[x].nome)
                     h=h+1
 
-        
-
         if h==0: 
             print("Aguardando Requisições")            
         else:
             y = input("Modificar item nº:")
-            if y == 's':
+            if y == :
                 Logistica.Main()
-        
             for x in range(len(lista)):
                     if x == int(y):
                         r = input("Sim(1)   Não(0)")
                         lista[x].entrega = r
         x = input("")
         Logistica.Main()
-
-Logistica.Main()
