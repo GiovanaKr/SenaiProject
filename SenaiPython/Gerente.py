1055fddd60eead9672020473bc142823f5322bf3
céu <60863899+GiovanaKr@users.noreply.github.com>
2021-11-03 21:21:24 -0300

    Update Gerente.py

--- SenaiPython/Gerente.py
+++ SenaiPython/Gerente.py
@@ -3,10 +3,6 @@ import os
 clear = lambda: os.system('cls')
 lista = ObjetoPedido.listaP
 
-#sair da def lista()
-#input incorreto simples
-#loop lista
-
 class Gerente:
     def Main():
         clear()
@@ -16,6 +12,7 @@ class Gerente:
         r = input(": ")
         if int(r) == 1:
             Gerente.Lista()
+            Gerente.Main()
         elif int(r) == 5:
             return
         else:
@@ -24,9 +21,9 @@ class Gerente:
     def Lista():
         clear()
         h = 0
-        print("presione 's' para sair")
+        print("presione '0' para sair")
         for x in range(len(lista)): #para cada item na lista
-            print(str(x)+" "+"Item: "+lista[x].qtd+" "+lista[x].nome)
+            print(str(x+1)+" "+"Item: "+lista[x].qtd+" "+lista[x].nome)
             if int(lista[x].aprovGen) == 2:
                 print("Aguardando verificação")
             elif int(lista[x].aprovGen) == 1:
@@ -38,23 +35,27 @@ class Gerente:
 
         if h == 0 :
             print("Aguardando requisições") 
+            f = input('')
+            return
         else:
             y = input("Modificar item nº:")
-            if y == 's':
-                Gerente.Main()
-            for x in range(len(lista)):
-                if x == int(y):
-                    print(lista[x].qtd + " " +lista[x].nome)
-                    r = input("Aprovar(1)   Reprovar(0)")
-                    if r == 's':
-                        Gerente.Main()    
-                    if  int(r) == 0:
-                        lista[x].justificava=input('Motivo:')
-                    if int(r) == 0 or int(r) == 1:
-                        lista[x].aprovGen = r 
-                    else:
-                        print("input incorreto")
-                        x = input("")
-                        Gerente.Lista()
+            d = int(y)-1 #numero certo
+            if int(y) == 0:
+                return
+            else:
+                for x in range(len(lista)):
+                    if x == d:
+                        print(lista[x].qtd + " " +lista[x].nome)
+                        r = input("Aprovar(1)   Reprovar(0)")
+                        if r == 's':
+                            Gerente.Main()            
+                        elif  int(r) == 0:
+                            lista[x].justificava=input('Motivo:')             
+                            lista[x].aprovGen = r
+                        elif int(r) == 1:
+                            lista[x].aprovGen = r 
+                        else:
+                            print("input incorreto")
+                            x = input("")
+                            Gerente.Lista()
         Gerente.Lista()
-Gerente.Main()
