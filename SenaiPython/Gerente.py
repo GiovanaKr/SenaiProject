e3d66046b62d00a06173038911edb091392e57a7
céu <60863899+GiovanaKr@users.noreply.github.com>
2021-11-09 21:31:17 -0300

    Update Gerente.py

--- SenaiPython/Gerente.py
+++ SenaiPython/Gerente.py
@@ -1,5 +1,6 @@
 import ObjetoPedido
 import os
+from datetime import datetime
 clear = lambda: os.system('cls')
 lista = ObjetoPedido.listaP
 estoque = ObjetoPedido.listaE
@@ -7,10 +8,6 @@ estoque = ObjetoPedido.listaE
 #pp = ObjetoPedido.Pedido('caneta','100')
 #pp.numero = 2
 #lista.append(pp)
-
-#ver estoque
-#modifica pedido
-#motivo modificação
 
 class Gerente:
     def Main():
@@ -33,18 +30,19 @@ class Gerente:
         print("presione '0' para sair")
         print('')
         for x in range(len(lista)): #para cada item na lista
-            print("requisição nº"+str(lista[x].numero))
-            print(lista[x].qtd+" "+lista[x].nome)
             if int(lista[x].aprovGen) == 2:
-                print("Aguardando verificação")
-            elif int(lista[x].aprovGen) == 1:
-                print("Aprovado")
-            else:
-                print("Negado")
-            print("")
-            h = h+1
+                print("requisição nº"+str(lista[x].numero))
+                print(lista[x].qtd+" "+lista[x].nome)
+                if int(lista[x].aprovGen) == 2:
+                    print("Aguardando verificação")
+                elif int(lista[x].aprovGen) == 1:
+                    print("Aprovado")
+                else:
+                    print("Negado")
+                print("")
+                h = h+1
 
-        if h == 0 :
+        if h == 0 :     #caso não tenha itens
             print("Aguardando requisições") 
             f = input('')
             return
@@ -57,23 +55,27 @@ class Gerente:
                     if x == int(y)-1:
                         print(lista[x].qtd + " " +lista[x].nome)
                         r = input("Aprovar(1)   Reprovar(0)     modificar(3)") #e modificar
-                        if  r == '0':
+                        if  r == '0':       #pedido negado
                             lista[x].justificativa=input('Motivo:')             
                             lista[x].aprovGen = r
+                            data = datetime.now()       #pega data
+                            lista[x].dataGen = data.strftime('%d/%m/%Y %H:%M') #salva data atual
 
-                        elif r == '1':
+                        elif r == '1':      #pedido aprovado
                             lista[x].aprovGen = r 
+                            data = datetime.now()       #pega data
+                            lista[x].dataGen = data.strftime('%d/%m/%Y %H:%M') #salva data atual
 
-                            #modificar pedido
-                        elif r == '3':
+                        elif r == '3':      #modificar pedido
                             lista[x].modifica = ('modificado['+lista[x].qtd+' '+lista[x].nome+' '+']')
                             print('modificação')
-    
                             lista[x].nome = input('nome:')
                             lista[x].qtd = input('quantidade:')
                             t = input('motivo: ')
                             lista[x].modifica = (lista[x].modifica + t)
-                            lista[x].aprovGen = '1'
+                            lista[x].aprovGen = '1'     #aprova pedido
+                            data = datetime.now()       #pega data
+                            lista[x].dataGen = data.strftime('%d/%m/%Y %H:%M') #salva data atual
 
                         else:
                             print("input incorreto")
