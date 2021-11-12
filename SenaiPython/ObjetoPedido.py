1b796c1f30a23536345605d7e7aa9f34e0d28e40
céu <60863899+GiovanaKr@users.noreply.github.com>
2021-11-12 20:55:31 -0300

    Update ObjetoPedido.py

--- SenaiPython/ObjetoPedido.py
+++ SenaiPython/ObjetoPedido.py
@@ -13,15 +13,39 @@ class Pedido:   #OBJETO
         self.dataPed = 0   #data pedido criado
 
         self.aprovGen = 2  #gerente
-        self.modifica = 0  #justificativa modificar produto
+        self.modifica = '0'  #justificativa modificar produto
         self.dataGen = 0   #data aprovação
         
 
         self.aprovCom = 2   #compras
         self.dataCom = 0    #data compra feita
 
-        self.log = 2       #logistica
-        self.entrega = 0   #entregue
-        self.dataLog = 0   #data chegada
+        self.log = 2         #logistica
+        self.dataLog = 0     #data chegada
+        self.emEstoque = 0   #este item esta no estoque
+        self.retirada = '0'  #quem retirou
 
-        self.justificativa = 0 #caso negado
+        self.justificativa = '0' #caso negado
+
+
+def ListaCompleta():
+    for obj in listaP:
+        print(obj.numero)
+
+        print(obj.nome)
+        print(obj.qtd)  
+        print(obj.dataPed) 
+
+        print(obj.aprovGen)
+        print(obj.modifica)
+        print(obj.dataGen)
+        
+        print(obj.aprovCom) 
+        print(obj.dataCom)
+
+        print(obj.log)
+        print(obj.dataLog)
+        print(obj.emEstoque)
+        print(obj.retirada)
+
+        print(obj.justificativa)
